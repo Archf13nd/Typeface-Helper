b9e4cc22a320963b0d5feae57cb32e28d9fd5f8f
unknown <crazeddemonanarch@gmail.com>
2022-04-13 09:18:58 +0100

    Initial commit: Starting files are from functioning prototype.

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import TextWindow from "./components/TextPreview/index.vue";
+export default {
+  data() {
+    return {
+      title: "The dead eye",
+      paragraph:
+        "Because Elden Ring is good. Like, cancel-your-plans, ignore-your-chores, glance-up-and-realize-it’s-three-in-the-morning good. It’s good in the kind of way you want to evangelize to practically everyone, which marks it as something of a departure from its immediate predecessors. ",
+    };
+  },
+  components: {
+    TextWindow,
+  },
+};
+</script>
+
+<template>
+  <div class="text-windows">
+    <text-window :title="title" :paragraph="paragraph"></text-window>
+    <text-window :title="title" :paragraph="paragraph"></text-window>
+    <text-window :title="title" :paragraph="paragraph"></text-window>
+    <text-window :title="title" :paragraph="paragraph"></text-window>
+    <text-window :title="title" :paragraph="paragraph"></text-window>
+    <text-window :title="title" :paragraph="paragraph"></text-window>
+  </div>
+</template>
+
+<style lang="scss">
+.text-windows {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
---
+++ src/components/TextPreview/index.vue
@@ -0,0 +1,123 @@
+<script>
+import Settings from "./Settings.vue";
+export default {
+  props: ["title", "paragraph"],
+  data() {
+    return {
+      isSettingsHidden: true,
+      currentTypeface: "",
+      activeFontSize: 16,
+      activeLeading: 1.3,
+      activeMeasure: 500,
+    };
+  },
+  components: {
+    Settings,
+  },
+  computed: {
+    getTypeface() {
+      return this.currentTypeface;
+    },
+    getFontFamily() {
+      let posOfEquals = this.currentTypeface.indexOf("=") + 1;
+      let ffString;
+      if (posOfEquals > 0) {
+        let posOfAnd = this.currentTypeface.indexOf("&");
+        ffString = this.currentTypeface.slice(posOfEquals, posOfAnd);
+      } else {
+        ffString = this.currentTypeface;
+      }
+      if (ffString.length > 1) {
+        this.importWebFont(ffString);
+        return `'${ffString}', serif`;
+      }
+    },
+    getFontSize() {
+      return this.activeFontSize + "px";
+    },
+    getMeasure() {
+      return this.activeMeasure + "px";
+    },
+    getLeading() {
+      return this.activeLeading;
+    },
+  },
+  methods: {
+    importWebFont(font) {
+      WebFont.load({
+        google: {
+          families: [`${font}`],
+        },
+      });
+    },
+    loadTypeface(e) {
+      this.currentTypeface = e;
+    },
+    loadFontSize(e) {
+      this.activeFontSize = +e;
+    },
+    loadLeading(e) {
+      this.activeLeading = +e;
+    },
+    loadMeasure(e) {
+      this.activeMeasure = +e;
+    },
+  },
+};
+</script>
+
+<template>
+  <div
+    class="text-preview"
+    :style="{
+      fontFamily: getFontFamily,
+      fontSize: getFontSize,
+      lineHeight: getLeading,
+    }"
+  >
+    <settings
+      @typeface="loadTypeface"
+      @fontSize="loadFontSize"
+      @leading="loadLeading"
+      @measure="loadMeasure"
+      :hideSettings="isSettingsHidden"
+    ></settings>
+    <button
+      class="settings-revealer"
+      @click="isSettingsHidden = !isSettingsHidden"
+    >
+      Settings
+    </button>
+
+    <div
+      class="text-preview__text"
+      :style="{
+        width: getMeasure,
+      }"
+    >
+      <h1 contenteditable="true">{{ title }}</h1>
+      <p contenteditable="true">{{ paragraph }}</p>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.text-preview {
+  display: flex;
+  margin: 2rem;
+  border: 1px solid #333;
+
+  &__text {
+    padding: 2rem;
+    width: inherit;
+  }
+}
+
+.settings-revealer {
+  position: absolute;
+  right: left;
+  font-size: initial;
+  line-height: initial;
+  display: none; //todo
+}
+</style>
